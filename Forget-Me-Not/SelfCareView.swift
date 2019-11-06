89febbd3a57cbd2fd3f21c8fc4a3461e6cfd2169
Sarah Shahinpour <sarahshahinpour@dhcp-wifi-8021x-168-122-3-228.bu.edu>
2019-11-06 15:41:33 -0500

    added self-care table programatically. Will redo in a storyboard.

---
+++ Forget-Me-Not/SelfCareView.swift
@@ -0,0 +1,14 @@
+//
+//  SelfCareView.swift
+//  Forget-Me-Not
+//
+//  Created by Sarah Shahinpour on 10/26/19.
+//  Copyright © 2019 Appollo. All rights reserved.
+//
+
+import Foundation
+
+import SwiftUI
+
+
+
