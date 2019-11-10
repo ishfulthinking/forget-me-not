46868ed9aa8797bc7c9682cc58257ce8aa2c502e
Ishmael Perez <ishmaelkperez@gmail.com>
2019-11-10 18:36:30 -0500

    Added basic welcome message

---
+++ forget-me-not/Components/SelfCare/SelfCareViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SelfCareViewController.swift
+//  forget-me-not
+//
+//  Created by Ishmael Perez on 11/10/19.
+//  Copyright © 2019 Appollo. All rights reserved.
+//
+
+import UIKit
+
+class SelfCareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
