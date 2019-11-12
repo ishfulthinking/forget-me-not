c5a15a7647d5c403af837b442746fd112a2c50b3
Ishmael Perez <ishmaelkperez@gmail.com>
2019-11-12 15:30:03 -0500

    [FMN-16] Added bottom tab bar view for navigation

--- forget-me-not/Components/Dashboard/DashboardViewController.swift
+++ forget-me-not/Components/Dashboard/DashboardViewController.swift
@@ -6,15 +6,37 @@
 //  Copyright © 2019 Appollo. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UITextFieldDelegate {
     // MARK: Properties
     @IBOutlet weak var welcomeText: UITextView!
     @IBOutlet weak var namePrompt: UITextView!
     @IBOutlet weak var nameTextField: UITextField!
+    var name: String?
+    
+    // MARK: Actions
+    @IBAction func checkTextFieldReturn(_ sender: Any) {
+        if nameIsValid(entered: nameTextField.text) {
+            self.name = nameTextField.text
+        }
+    }
+    
+    private func sendGreeting() {
+        guard let welcomeText = welcomeText else { return }
+        welcomeText.text = "Hello " + name!
+    }
+    
+    private func nameIsValid(entered: String?) -> Bool {
+        if entered == nil { return false }
+        return entered!.count > 0
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 113, green: 80, blue: 143, alpha: 1.0)
+        self.title = "Dashboard"
+        print("Loaded dashboard view controller.")
     }
 }
--- forget-me-not/Components/SelfCare/SelfCareViewController.swift
+++ forget-me-not/Components/SelfCare/SelfCareViewController.swift
@@ -11,5 +11,8 @@ import UIKit
 class SelfCareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 110, green: 100, blue: 90, alpha: 1.0)
+        self.title = "Self Care"
+        print("Loaded self care view controller.")
     }
 }
